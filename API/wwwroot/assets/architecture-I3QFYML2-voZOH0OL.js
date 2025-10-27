90307f3817a3fcb0abf8e2e9a1112ece47a5cf65
Emad Radwan <eradwan1967@gmail.com>
2025-10-27 12:23:26 +0300

    build 27-10 /4

---
+++ API/wwwroot/assets/architecture-I3QFYML2-voZOH0OL.js
@@ -0,0 +1,1 @@
+import{A as m,e as p}from"./mermaid-parser.core-5k32b2Hb.js";import"./index-GCvzUHIO.js";import"./defaults-yMk8QfMK.js";import"./_baseClone-QvXfNBJ-.js";import"./clone-29XHbqsx.js";export{m as ArchitectureModule,p as createArchitectureServices};
