ce150940d27981802759f2db05b7c6b875ebae19
Emad Radwan <eradwan1967@gmail.com>
2026-01-13 20:13:24 +0200

    build 13-1 /9

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -68,11 +68,7 @@ public static class ApplicationServiceExtensions
 
         services.AddMvc().AddJsonOptions(options =>
         {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-            options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-            // No need for custom DateTime converter if you do this:
-            options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-            // System.Text.Json already handles ISO 8601 with Z correctly by default
+            options.JsonSerializerOptions.Converters.Add(new DateTimeConverter());
         });
 
         services.AddScoped<IPdfGenerationService, PdfGenerationService>();
--- API/Program.cs
+++ API/Program.cs
@@ -261,3 +261,16 @@ public class DateTimeConverter : JsonConverter<DateTime>
         writer.WriteStringValue(DateTime.SpecifyKind(value, DateTimeKind.Utc));
     }
 }
+
+public class UtcDateTimeConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return DateTime.Parse(reader.GetString()!);  // or more strict parsing
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss") + "Z");
+    }
+}
