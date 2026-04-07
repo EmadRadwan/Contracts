7e380b611d5bc7decf7bfb3cb9c0c90a560222ed
Emad Radwan <eradwan1967@gmail.com>
2026-04-07 15:27:44 +0200

    build 7-4 /6

---
+++ Persistence/Migrations/20260407125454_AddApprovedByPartyId.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApprovedByPartyId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "APPROVED_BY_PARTY_ID",
+                table: "PAYMENT",
+                type: "varchar(36)",
+                unicode: false,
+                maxLength: 36,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "PAYMENT_APPR_PTY",
+                table: "PAYMENT",
+                column: "APPROVED_BY_PARTY_ID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PAYMENT_APPROVED_BY_PARTY",
+                table: "PAYMENT",
+                column: "APPROVED_BY_PARTY_ID",
+                principalTable: "PARTY",
+                principalColumn: "PARTY_ID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PAYMENT_APPROVED_BY_PARTY",
+                table: "PAYMENT");
+
+            migrationBuilder.DropIndex(
+                name: "PAYMENT_APPR_PTY",
+                table: "PAYMENT");
+
+            migrationBuilder.DropColumn(
+                name: "APPROVED_BY_PARTY_ID",
+                table: "PAYMENT");
+        }
+    }
+}
