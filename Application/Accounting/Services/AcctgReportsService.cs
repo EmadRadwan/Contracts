ea1cc2640cfde15b97021ae26464a834d2d6f0bd
Emad Radwan <eradwan1967@gmail.com>
2026-05-03 14:56:24 +0300

    build 3-5 /3

--- Application/Accounting/Services/AcctgReportsService.cs
+++ Application/Accounting/Services/AcctgReportsService.cs
@@ -171,11 +171,11 @@ public class AcctgReportsService : IAcctgReportsService
                 //    using our equivalent of the 'computeGlAccountBalanceForTimePeriod' service.
                 foreach (var organizationGlAccount in organizationGlAccounts)
                 {
-                    if (organizationGlAccount.GlAccountId != "110100")
+                    /*if (organizationGlAccount.GlAccountId != "110100")
                     {
                         Console.WriteLine($"Skipping GlAccountId: {organizationGlAccount.GlAccountId}");
                         continue;
-                    }
+                    }*/
 
                     var accountBalance = await ComputeGlAccountBalanceForTimePeriod(
                         organizationGlAccount.OrganizationPartyId,
